1454f165b6338615f1d6ebc3f263101fe9e7791e
Sebastian Spindler (kwerdenker) <thinkkwer@gmail.com>
2018-11-09 20:09:49 +0100

    Add RGB underglow support to SINGA
    Custom driver taken from ps2avrGB, on which the SINGA RGB implementation is based, modified to support RGBW

--- keyboards/singa/singa.c
+++ keyboards/singa/singa.c
@@ -20,15 +20,52 @@
 #include "i2c.h"
 #include "quantum.h"
 
+#ifdef RGBLIGHT_ENABLE
+#include "rgblight.h"
+
+#ifdef RGBW
+#define BYTE_PER_LED 4
+#else
+#define BYTE_PER_LED 3
+#endif
+
+extern rgblight_config_t rgblight_config;
+
+// custom RGB driver
+void rgblight_set(void) {
+  if (!rgblight_config.enable) {
+    for (uint8_t i=0; i<RGBLED_NUM; i++) {
+      led[i].r = 0;
+      led[i].g = 0;
+      led[i].b = 0;
+#ifdef RGBW
+      led[i].w = 0;
+#endif
+    }
+  }
+
+  i2c_init();
+  i2c_send(0xb0, (uint8_t*)led, BYTE_PER_LED * RGBLED_NUM);
+}
+#endif // RGBLIGHT_ENABLE
 
 // for keyboard subdirectory level init functions
 // @Override
 void matrix_init_kb(void) {
+#ifdef RGBLIGHT_ENABLE
+    if (rgblight_config.enable) {
+    i2c_init();
+    i2c_send(0xb0, (uint8_t*)led, BYTE_PER_LED * RGBLED_NUM);
+  }
+#endif
   // call user level keymaps, if any
   matrix_init_user();
 }
 
 void matrix_scan_kb(void) {
+#ifdef RGBLIGHT_ENABLE
+  rgblight_task();
+#endif
   matrix_scan_user();
   /* Nothing else for now. */
 }
