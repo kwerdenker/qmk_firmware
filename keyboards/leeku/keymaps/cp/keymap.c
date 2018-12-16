363bf5ee15d0c169f64144118fdabd252412515a
Sebastian Spindler (kwerdenker) <thinkkwer@gmail.com>
2018-12-17 00:22:58 +0100

    Fixed left alt key assignment

--- keyboards/leeku/keymaps/cp/keymap.c
+++ keyboards/leeku/keymaps/cp/keymap.c
@@ -9,14 +9,14 @@
     K3A, K3B, K3C, K3D, K3E, K3F, K3G, K3H, K3I, K3J, K3K, K3L, K3M,           K3Q, K3R, K3S, K4T,\
     K2A, K2B, K2C, K2D, K2E, K2F, K2G, K2H, K2I, K2J, K2K, K2L, K2M, K2N,      K2Q, K2R, K2S, K2T,\
     K1A, K1B, K1C, K1D, K1E, K1F, K1G, K1H, K1I, K1J, K1K, K1L, K1M,      K1O, K1Q, K1R, K1S, K0T,\
-    K0A, K0B,                     K0G,                K0J,      K0M, K0N, K0O, K0P, K0R, K0S, K1T \
+    K0A, K0C,                     K0G,                K0J,      K0M, K0N, K0O, K0P, K0R, K0S, K1T \
 ) { \
   { K5A, K5B, K5C,   K5D,   K5E,   K5F, K5G, K5H, K5I, K5J, K5K, K5L, K5M,   KC_NO, KC_NO, K5P,           K5Q,   KC_NO, KC_NO, KC_NO }, \
   { K4A, K4B, K4C,   K4D,   K4E,   K4F, K4G, K4H, K4I, K4J, K4K, K4L, K4M,   KC_NO, K4O,   KC_NO,         K4Q,   K4R,   K4S,   K4T     }, \
   { K3A, K3B, K3C,   K3D,   K3E,   K3F, K3G, K3H, K3I, K3J, K3K, K3L, K3M,   KC_NO, K3O,   K3P,           K3Q,   K3R,   K3S,   KC_NO   }, \
   { K2A, K2B, K2C,   K2D,   K2E,   K2F, K2G, K2H, K2I, K2J, K2K, K2L, K2M,   K2N,   K2O,   K2P,           K2Q,   K2R,   K2S,   K2T     }, \
   { K1A, K1B, K1C,   K1D,   K1E,   K1F, K1G, K1H, K1I, K1J, K1K, K1L, K1M,   KC_NO, K1O,   KC_NO,         K1Q,   K1R,   K1S,   K1T     }, \
-  { K0A, K0B, KC_NO, KC_NO, KC_NO, KC_NO,    K0G, KC_NO,  KC_NO, K0J, KC_NO, KC_NO, K0M,   K0N, K0O, K0P, KC_NO, K0R,   K0S,   K0T  }  \
+  { K0A, KC_NO, K0C, KC_NO, KC_NO, KC_NO,    K0G, KC_NO,  KC_NO, K0J, KC_NO, KC_NO, K0M,   K0N, K0O, K0P, KC_NO, K0R,   K0S,   K0T  }  \
 }
 
 #define TAPPING_TERM 120 //time in microseconds a key is counted as tapped
