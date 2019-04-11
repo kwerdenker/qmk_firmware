57db982e03df79ea4bfece17c2b81aa129543aa9
Sebastian Spindler <sebastian.spindler@gmail.com>
2019-04-11 10:25:51 +0200

    Change layer highlighting to fit DSA Galaxy Class caps

--- keyboards/westfoxtrot/cypher/keymaps/kwer/keymap.c
+++ keyboards/westfoxtrot/cypher/keymaps/kwer/keymap.c
@@ -23,11 +23,12 @@
 #define _LE 2
 
 #define SPECIAL LT(_FN, DE_CIRC)    //capslock layer switch + stargate key on tap
+#define ALT_TAB LALT(KC_TAB)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_DL] = LAYOUT_iso (
-      KC_ESC,  KC_1,    KC_2,  KC_3,  KC_4,  KC_5,  KC_6,  KC_7,  KC_8,  KC_9,    KC_0,   DE_SS,   DE_ACUT, KC_BSPC, _______,     KC_NLCK, KC_PSCR,KC_PSLS,KC_PAST,
+      KC_ESC,  KC_1,    KC_2,  KC_3,  KC_4,  KC_5,  KC_6,  KC_7,  KC_8,  KC_9,    KC_0,   DE_SS,   DE_ACUT, KC_BSPC, _______,     KC_DEL,  KC_PSCR,KC_PSLS,KC_PAST,
       KC_TAB,  KC_Q,    KC_W,  KC_E,  KC_R,  KC_T,  DE_Z,  KC_U,  KC_I,  KC_O,    KC_P,   DE_UE,   DE_PLUS,                       KC_P7,   KC_P8,  KC_P9,  KC_PMNS,
       SPECIAL, KC_A,    KC_S,  KC_D,  KC_F,  KC_G,  KC_H,  KC_J,  KC_K,  KC_L,    DE_OE,  DE_AE,   DE_HASH, KC_ENT,               KC_P4,   KC_P5,  KC_P6,  KC_PPLS,
       KC_LSFT, DE_LESS, DE_Y,  KC_X,  KC_C,  KC_V,  KC_B,  KC_N,  KC_M,  KC_COMM, KC_DOT, DE_MINS, KC_RSFT,        KC_UP,         KC_P1,   KC_P2,  KC_P3,  KC_PENT,
@@ -35,8 +36,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [_FN] = LAYOUT_iso (
-      DE_RING, KC_F1,   KC_F2,   KC_F3,   KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,  KC_F11,  KC_F12,  KC_DEL, _______,      _______, _______, _______, _______,
-      _______, _______, KC_UP,   _______, _______,  _______,  _______,  _______,  _______,  _______,  _______, _______, _______,                        _______, _______, _______, _______,
+      DE_RING, KC_F1,   KC_F2,   KC_F3,   KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,  KC_F11,  KC_F12,  KC_DEL, _______,       KC_NLCK, _______, _______, _______,
+      ALT_TAB, _______, KC_UP,   _______, _______,  _______,  _______,  _______,  _______,  _______,  _______, _______, _______,                        _______, _______, _______, _______,
       _______, KC_LEFT, KC_DOWN, KC_RGHT, _______,  _______,  _______,  _______,  _______,  _______,  _______, _______, _______, _______,               _______, _______, _______, _______,
       _______, _______, _______, _______, _______,  _______,  _______,  _______,  _______,  _______,  _______, _______, _______, _______,               _______, _______, _______, _______,
       _______, _______, _______,             KC_LGUI,                _______, _______, _______,                         _______, _______, _______,               _______, _______, _______
@@ -67,13 +68,32 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 void led_set_user(uint8_t usb_led) {
   // Insert custom handling for CAPS_LOCK, NUM_LOCK, SCROLL_LOCK here
-      if (IS_LED_ON(usb_led, USB_LED_NUM_LOCK)) {
-        writePinHigh(F4);
-        writePinHigh(F1);
-        writePinHigh(F5);
-    } else {
-        writePinLow(F4);
-        writePinLow(F1);
-        writePinLow(F5);
-    }
+  if (IS_LED_ON(usb_led, USB_LED_NUM_LOCK)) {
+    writePinHigh(F4);
+    writePinHigh(F1);
+    writePinHigh(F5);
+  } else {
+    writePinLow(F4);
+    writePinLow(F1);
+    writePinLow(F5);
+  }
+}
+
+uint32_t layer_state_set_user(uint32_t state) {
+  switch (biton32(state)) {
+  case _FN:
+    rgblight_enable_noeeprom();
+    rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_LIGHT);
+    rgblight_setrgb(0x83, 0x5A, 0xED);
+    break;
+  case _LE:
+    rgblight_enable_noeeprom();
+    rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_LIGHT);
+    rgblight_setrgb(0xFF, 0xF2, 0x00);
+    break;
+  default: //  for any other layers, or the default layer
+    rgblight_disable_noeeprom();
+    break;
+  }
+  return state;
 }
